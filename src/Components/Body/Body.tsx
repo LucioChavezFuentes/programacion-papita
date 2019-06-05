6d4ecb895cd2a29d98951622ed1067a9a8c4be3b
LucioChavezFuentes <48106652+LucioChavezFuentes@users.noreply.github.com>
2019-06-05 14:33:36 -0500

    NavLink with Transitions Complete and Stable

---
+++ src/Components/Body/Body.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import {BrowserRouter , Route, Switch} from 'react-router-dom';
+import { CSSTransition, TransitionGroup} from 'react-transition-group';
---
+++ src/Components/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+
+    return (
+        <div className='page'>
+            <p>Contact</p>
+        </div>
+    )
+}
+
+export default Contact;
