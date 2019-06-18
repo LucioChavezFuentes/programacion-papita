1127e0db76179a3040fa4f386ea2162d060b2e80
Lucio Chavez <luciobertinchavez@gmail.com>
2019-06-17 19:28:05 -0500

    Modify Home Component

--- src/Components/Body/Home/Home.tsx
+++ src/Components/Body/Home/Home.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import {InputGroup} from 'react-bootstrap'
 
 const Home = () => {
 
     return (
         <div className='page'>
-            <p>Aprender a progrmar es más fácil de lo que crees.</p>
+            <div className='greeting'>
+                <div>
+                    <p>!Cualquiera puede programar! </p>
+                    <p>Es más que una carrera... Es una ventura.</p>
+                </div>
+
+                <div>
+                    <p>Y aprender aquí está papita.</p>
+                </div>
+
+            </div>
+
+            <div className='registration'>
+                <div>
+                    <p>!Resgístrate Gartis!</p>
+                </div>
+
+                <div>
+                    <p>Email</p>
+                    <input />
+                </div>
+
+                <div>
+                    <p>Password</p>
+                    <input placeholder="T" />
+
+                </div>
+                
+
+            </div>
         </div>
+
+        
     )
 }
 
